05f339ecce4af55070099e9bbf7ae77abb54f3ae
marizaswart <mariza.swart@gmail.com>
2018-02-03 20:46:20 +0100

    Consume API endpoint for plants collection

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {getPlants} from './api/plants';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props)
+        this.state = {plants: []}
+    }
+
+    componentDidMount() {
+        getPlants().then(data => {
+            this.setState({plants: data.entity.data})
+        })
+    }
+
+    render() {
+        const plantList = this.state.plants.map(plant => {
+            return <li key={plant.id}>{plant.attributes.name}</li>
+        });
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Welcome to PlantEd</h1>
+                </header>
+                <ul style={{listStyle: 'none'}}>
+                    {plantList}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default App;
