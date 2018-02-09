09689d95bbd9f4a27c5ae15d39d98b3c76965703
marizaswart <mariza.swart@gmail.com>
2018-02-09 01:02:22 +0100

    WIP user can add plants to their list

--- src/Components/AddPlant.js
+++ src/Components/AddPlant.js
@@ -4,9 +4,13 @@ import { Button } from 'reactstrap';
 // import SelectButton from 'SelectButton';
 
 class AddPlant extends Component {
-    constructor() {
-      super();
-      this.state = {plants: []}
+    constructor(props) {
+      super(props);
+      this.state = {
+          plants: []
+      };
+
+        this.onClick = this.onClick.bind(this);
     }
 
     componentDidMount() {
@@ -15,34 +19,38 @@ class AddPlant extends Component {
         })
     }
 
-    createPlant(e) {
-        e.preventDefault();
-        let plant = this.state.plants['0'];
-        debugger;
-        if (typeof plant === 'string' && plant.length > 0) {
-            this.props.selectPlant(plant);
-        }
-    }
+    // createPlant(e) {
+    //     e.preventDefault();
+    //     let plant = this.state.plants['0'];
+    //     debugger;
+    //     if (typeof plant === 'string' && plant.length > 0) {
+    //         this.props.selectPlant(plant);
+    //     }
+    // }
+    //
+    // selectPlant(plant) {
+    //     let timestamp = (new Date()).getTime();
+    //     this.state.plants['plant-' + timestamp] =plant;
+    //     this.setState({plants : this.state.plants });
+    // }
 
-    selectPlant(plant) {
-        let timestamp = (new Date()).getTime();
-        this.state.plants['plant-' + timestamp] =plant;
-        this.setState({plants : this.state.plants });
+    onClick(event) {
+        this.setState({value: event.target.value});
     }
 
     render() {
       const plantOptions = this.state.plants.map(plant => {
-          return <option key={plant.id} value="plant">{plant.attributes.name}</option>
+          return <option key={plant.id} value={this.state.value}>{plant.attributes.name}</option>
       });
         return (
             <div>
                 <h3>Select your plants from the list here:</h3>
-                <select ref="plant">
+                <select value={this.state.value} onChange={this.onClick} ref="plant">
                     {plantOptions}
                 </select>
-                <Button size="sm" onClick={this.createPlant.bind(this)}>Add Plant</Button>
+                {/*<Button size="sm" onClick={this.createPlant.bind(this)}>Add Plant</Button>*/}
                 <ul>
-                    <li>{this.state.selectPlant}</li>
+                    <li>{this.state.value}</li>
                 </ul>
             </div>
         );
