7bde418e0a851be069b4b8ce612b2eb3f782057e
marizaswart <mariza.swart@gmail.com>
2018-02-11 10:10:43 +0100

    Style select item

--- src/Components/ShowUser.js
+++ src/Components/ShowUser.js
@@ -1,15 +1,11 @@
 import React, {Component} from 'react';
 import {getUser} from '../Modules/User';
 import EventEmitter from '../Modules/EventEmitter';
-import { Collapse } from 'reactstrap';
-
 
 class ShowUser extends Component {
     constructor() {
         super();
-        this.toggle = this.toggle.bind(this);
         this.state = {user: [], isLoading: true,
-            collapse: false
         }
     }
 
@@ -28,10 +24,6 @@ class ShowUser extends Component {
         })
     }
 
-    toggle() {
-        this.setState({ collapse: !this.state.collapse });
-    }
-
 
     render() {
 
@@ -42,30 +34,17 @@ class ShowUser extends Component {
         } else {
             const plantList = user.relationships.plants.data.map(plant => {
                 return (
-                    <div>
-                        <Collapse multi-collapse isOpen={this.state.collapse}>
-                            <div className="container-fluid" onClick={this.toggle}>
-                                {/*<p>{plant.image}</p>*/}
-                                <div>
-                                    <h5>{plant.name}</h5>
-                                    <p>{plant.description}</p>
-                                </div>
-                            </div>
-                        </Collapse>
-                        <div className="card" onClick={this.toggle}>
-                            {/*<p>{plant.image}</p>*/}
-                            <div>
-                                <h5>{plant.name}</h5>
-                                <p>{plant.description}</p>
-                            </div>
+                    <div className="card">
+                        {/*<p>{plant.image}</p>*/}
+                        <div>
+                            <h5>{plant.name}</h5>
+                            <p>{plant.description}</p>
                         </div>
                     </div>
                 )
             });
             return (
                 <div>
-                    {/*<h3>User</h3>*/}
-                    {/*<p>{user.attributes.email}</p>*/}
                     <div className="container">
                         {plantList}
                     </div>
